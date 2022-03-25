88b0b446e786c3651ab1ee6f609684a79a240b74
tosigame <tosigame@gmail.com>
2022-03-25 11:50:09 +0100

    Week 3 1

--- Week3/cs108/Main.java
+++ Week3/cs108/Main.java
@@ -10,9 +10,36 @@ public final class Main {
     public static void main(String[] args) throws IOException {
         BufferedImage image = ImageIO.read(new File("Week3/pskov.png"));
         String text = Steganographer.extract(image);
-       // System.out.println("Texte extrait de l'image :");
+        int bit=0;
+        String bits="10000010101010100101000010010000";
+
+        int rgb=(image.getRGB(100,100));
+        int red = (rgb>>16)&0x0ff;
+        int green=(rgb>>8) &0x0ff;
+        int blue= (rgb)    &0x0ff;
+        /*System.out.printf("[18]blue: %s\n",Integer.toBinaryString(blue));
+        System.out.printf("[19]red: %s\n",Integer.toBinaryString(red));
+        System.out.printf("[20]green: %s\n",Integer.toBinaryString(green)); //11111111010110110110111110000010
+        System.out.println(String.format("%32s", Integer.toBinaryString((image.getRGB(100,100)))).replace(' ', '0'));
+        int mask1 = 1;
+        System.out.println(String.format("%32s",Integer.toBinaryString(mask1 | rgb)).replace(' ','0'));
+        System.out.println();
+        for (int i = 0; i < 32; i++) {
+            if(Integer.parseInt(String.valueOf(bits.charAt(i)))==0){
+                System.out.println("==0 "+String.valueOf(bits.charAt(i)));
+                System.out.println(String.format("%32s", Integer.toBinaryString((rgb & ~mask1))).replace(' ', '0'));
+            }else{
+                System.out.println("==1 "+String.valueOf(bits.charAt(i)));
+                System.out.println(String.format("%32s", Integer.toBinaryString((rgb | mask1))).replace(' ', '0'));
+            }
+
+        }
+
+         */
+
+      // System.out.println("Texte extrait de l'image :");
         //System.out.println(text);
-        BufferedImage newImage = Steganographer.insert(image,"the hidden text");
+        BufferedImage newImage = Steganographer.insert(image,"the hidden text yooo did it workl lam");
         ImageIO.write(newImage, "png", new File("new_image.png"));
 
         System.out.println(Steganographer.extract(newImage));
--- Week3/cs108/Steganographer.java
+++ Week3/cs108/Steganographer.java
@@ -36,21 +36,28 @@ public final class Steganographer {
         //System.out.println();
         StringBuilder stringBuilder=new StringBuilder();
         for (int i = 0; i < letters.length; i++) {
-            stringBuilder.append(String.format("%16s", Integer.toBinaryString(i)).replace(' ', '0'));
+            stringBuilder.append(String.format("%16s", Integer.toBinaryString(letters[i])).replace(' ', '0'));
         }
         for (int i = letters.length; i < outImage.getHeight()*outImage.getWidth(); i++) {
             stringBuilder.append("0");
         }
         String binarySequence= stringBuilder.toString();
+        System.out.println(binarySequence);
+        int mask1=1;
        // System.out.println(binarySequence);
-        for(int y=0; y < image.getHeight(); y++){
-            for(int x=0; x < image.getWidth(); x++){
-                int rgb = image.getRGB(x, y);
-                int bit = (Integer.parseInt(String.valueOf(binarySequence.charAt(outImage.getHeight()*y+x))) >> --bitI) & 1;
-                outImage.setRGB(x, y, (rgb & ~1) | bit);
-                outImage.setRGB(x,y,(rgb & ~1) | bit);
+        for(int y=0; y < outImage.getHeight(); y++){
+            for(int x=0; x < outImage.getWidth(); x++){
+                int rgb = outImage.getRGB(x, y);
+                if(Integer.parseInt(String.valueOf(binarySequence.charAt(y* outImage.getHeight()+x)))==0){
+                    //System.out.println("0");
+                    outImage.setRGB(x,y,rgb & ~mask1);
+                }else{
+                  //  System.out.println("1");
+                    outImage.setRGB(x,y,rgb | mask1 );
+                }
             }
         }
+
         return outImage;
     }
 }
