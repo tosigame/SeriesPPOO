78d2dd460e01161240ad815a1e4f48eb24ac52a6
tosigame <tosigame@gmail.com>
2022-03-26 15:56:06 +0100

    Week 4 2 start

---
+++ WeekXforRandomTests/src/cs108/Main.java
@@ -0,0 +1,12 @@
+package cs108;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String alphabet ="abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < alphabet.length(); i++) {
+            System.out.println(i+": "+ alphabet.charAt(i));
+        }
+
+    }
+}
